d0036cd9437b2f64a015cc253f9780df51fc2e80
Ilja-Prihach <piratehnik9508@gmail.com>
2025-11-12 22:33:11 +0300

    min max value in array

---
+++ codewars/sprint5/7kuy/max.js
@@ -0,0 +1,19 @@
+// В массиве найти максимальное число
+
+const numbers = [1, 45, 66, 2, 4, 6, -2, -10, -100, 100]
+let minValue = numbers[0]
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] < minValue) {
+        minValue = numbers[i]
+    }
+}
+console.log(minValue)
+
+const numbers = [1, 45, 66, 2, 4, 6, -2, -10, -100, 100]
+let maxValue = numbers[0]
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > maxValue) {
+        maxValue = numbers[i]
+    }
+}
+console.log(maxValue)
