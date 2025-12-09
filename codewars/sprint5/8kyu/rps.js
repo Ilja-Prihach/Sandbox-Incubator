b58130198f1df22fa57827e858164ef88ea7a625
Ilja-Prihach <piratehnik9508@gmail.com>
2025-12-09 22:24:28 +0300

    new kata

---
+++ codewars/sprint5/8kyu/rps.js
@@ -0,0 +1,24 @@
+// Description:
+//     Rules of the "Rock, Paper, Scissors" game are:
+//
+//     Rock beats Scissors,
+//     Scissors beat Paper,
+//     Paper beats Rock,
+//     Two identical moves are a draw.
+//     Let's play! You will be given valid moves of two Rock, Paper, Scissors players, '
+// 'and have to return which player won: "Player 1 won!" for player 1, and "Player 2 won!" ' +
+// 'for player 2. In case of a draw return Draw!
+
+function rps(p1, p2) {
+    if (p1 === p2) {
+        return "Draw!";
+    }
+    if (
+        (p1 === "rock" && p2 === "scissors") ||
+        (p1 === "scissors" && p2 === "paper") ||
+        (p1 === "paper" && p2 === "rock")
+    ) {
+        return "Player 1 won!";
+    }
+    return "Player 2 won!";
+}
