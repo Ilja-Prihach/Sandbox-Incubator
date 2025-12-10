532bb45161551f440ff97b0c9482a60f8ec7a63b
Ilja-Prihach <piratehnik9508@gmail.com>
2025-12-10 23:22:06 +0300

    new kata

---
+++ codewars/sprint5/6kuy/lettersToNumbers.js
@@ -0,0 +1,33 @@
+// Sentence Calculator
+// Calculate the total score (sum of the individual character scores) of a sentence given the following score rules for each allowed group of characters:
+//
+//     Lower case [a-z]: 'a'=1, 'b'=2, 'c'=3, ..., 'z'=26
+// Upper case [A-Z]: 'A'=2, 'B'=4, 'C'=6, ..., 'Z'=52
+// Digits [0-9] their numeric value: '0'=0, '1'=1, '2'=2, ..., '9'=9
+// Other characters: 0 value
+// Note: input will always be a string
+
+
+
+function lettersToNumbers(s) {
+    const alphabets = 'abcdefghijklmnopqrstuvwxyz'
+    const alphabetArray = alphabets.split('')
+    let totalCount = 0
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i]
+        const lowerChare = char.toLowerCase()
+        const index = alphabetArray.indexOf(lowerChare)
+
+        if  (index !== -1){
+            if (char === alphabetArray[index]){
+                totalCount += (index + 1)
+            } else{
+                totalCount += (index + 1) * 2
+            }
+        }
+        else if (char >= '0' && char <= '9') {
+            totalCount += +char
+        }
+    }
+    return totalCount
+}
