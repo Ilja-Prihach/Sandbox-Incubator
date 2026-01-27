f484765b773eed441273973fadf44c2c9c727107
Ilja-Prihach <piratehnik9508@gmail.com>
2026-01-27 23:21:11 +0300

    new kata

--- codewars/sprint5/8kyu/invert.js
+++ codewars/sprint5/8kyu/invert.js
@@ -4,3 +4,9 @@
 //     [1, -2, 3, -4, 5] --> [-1, 2, -3, 4, -5]
 //     [] --> []
 // You can assume that all values are integers. Do not mutate the input array.
+
+
+function invert(array) {
+    if (array.length === 0) return array;
+    return array.map(i => -i)
+}
