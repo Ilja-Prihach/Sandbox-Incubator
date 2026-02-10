dfb174cf1c03120d1e5239898089d48ead2f5b3a
Ilja-Prihach <piratehnik9508@gmail.com>
2026-02-10 19:07:44 +0300

    kata solution

--- codewars/sprint5/6kuy/solve_a_b.js
+++ codewars/sprint5/6kuy/solve_a_b.js
@@ -8,3 +8,35 @@
 //     Tests go up to about index 20000.
 //
 // More examples in test cases. Good luck!
+
+
+
+function solve(a, b) {
+    const need = a + b;
+
+    let s = "";
+    const primes = [2];
+    s += "2";
+
+    let n = 3;
+
+    function isPrime(x) {
+        const limit = Math.floor(Math.sqrt(x));
+        for (let i = 0; i < primes.length; i++) {
+            const p = primes[i];
+            if (p > limit) break;
+            if (x % p === 0) return false;
+        }
+        return true;
+    }
+
+    while (s.length < need) {
+        if (isPrime(n)) {
+            primes.push(n);
+            s += String(n);
+        }
+        n += 2;
+    }
+
+    return s.slice(a, a + b);
+}
