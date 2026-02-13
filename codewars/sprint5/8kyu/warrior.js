5d40e6c20405593f7fee41f22fd7b007ab66a093
Ilja-Prihach <piratehnik9508@gmail.com>
2026-02-13 22:13:14 +0300

    kata solution

--- codewars/sprint5/8kyu/warrior.js
+++ codewars/sprint5/8kyu/warrior.js
@@ -20,3 +20,15 @@
 // Warrior.prototype.toString = function(){
 //     return "Hi! my name's "+this.name();
 // }
+
+function Warrior(n){
+    let name = n;
+    this.name = function(n){
+        if( n ) name=n;
+        return name;
+    }
+}
+
+Warrior.prototype.toString = function(){
+    return "Hi! my name's "+this.name();
+}
