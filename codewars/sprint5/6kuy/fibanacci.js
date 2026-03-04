464878733f81e59f73e674ac2e31e8e42e2ac2a6
Ilja-Prihach <piratehnik9508@gmail.com>
2026-03-04 20:16:34 +0300

    kata solution

--- codewars/sprint5/6kuy/fibanacci.js
+++ codewars/sprint5/6kuy/fibanacci.js
@@ -9,3 +9,19 @@
 //     0 --> 0
 // 33 --> 10
 // 25997544 --> 19544084
+
+function fibonacci(max) {
+    let sum = 0
+    let a= 0
+    let b = 1
+
+    while (a < max) {
+        if (a % 2 === 0) {
+            sum += a
+        }
+        let next = a + b
+        a = b
+        b = next
+    }
+    return sum
+}
