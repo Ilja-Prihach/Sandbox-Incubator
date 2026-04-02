cac6b0fe5436f3ce8701dcd8a99a1554fc2f181a
Ilja-Prihach <piratehnik9508@gmail.com>
2026-04-02 23:44:16 +0300

    kata solution

--- codewars/sprint5/8kyu/tripleTrouble.js
+++ codewars/sprint5/8kyu/tripleTrouble.js
@@ -7,5 +7,9 @@
 
 
 function tripleTrouble(one, two, three){
-    //Solution
+    let result = ""
+    for(let i=0; i<one.length; i++){
+        result += one[i] + two[i] + three[i]
+    }
+    return result;
 }
